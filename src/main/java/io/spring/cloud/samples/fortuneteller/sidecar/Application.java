7b8b457b3a8c4c4277bc7b761723ca19d5232d55
mmcvay-pivotal <mmcvay@pivotal.io>
2016-02-09 10:10:32 +0000

    first commit

---
+++ src/main/java/io/spring/cloud/samples/fortuneteller/sidecar/Application.java
@@ -0,0 +1,14 @@
+package io.spring.cloud.samples.fortuneteller.sidecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+@SpringBootApplication
+@EnableSidecar
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
